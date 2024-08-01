47b2746eb63cd133187a1329226b4f66db4f14e4
Naveen Kumar Lakkaram <naveenkumarlakkaram@Naveens-Air.lan>
2024-07-31 22:03:23 -0400

    At posts

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import {Outlet} from "react-router-dom";
-import Register from '../pages/Register';
 
 
 const Layout = () => {
@@ -10,69 +9,6 @@ const Layout = () => {
     <>
         <Header/>
             <Outlet/>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!v
-
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            v
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!v
-
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            v
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!v
-
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi quasi officia quae tempora corrupti ad quis voluptate numquam saepe, deleniti delectus ab perspiciatis velit dolor illum doloribus nam sed!
-            
         <Footer/>
     </>
   )
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <section className='error-page'>
+      <div className='center'>
+        <Link to="/" className='btn primary'>Go Back Home</Link>
+        <h2>Something is fishy!!! Please go back home</h2>
+      </div>
+    </section>
   )
 }
 
